92bfada3ec1a1d9c8cc9cbe0ce05a0fce45e6983
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2016-08-25 14:17:50 +0300

    Implements streaming KMeans.

--- src/main/scala/WordCrawler.scala
+++ src/main/scala/WordCrawler.scala
@@ -3,28 +3,45 @@ import org.apache.spark._
 import org.apache.spark.rdd._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.dstream._
-import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.linalg.{Vectors, Vector}
+import org.apache.spark.mllib.clustering.StreamingKMeans
 
 object WordCrawler {
   def replaceEmpty(s: String): String = {
     if (s.isEmpty) "0" else s
   }
 
+  def toLabelVectorTup(s: String): (Double, Vector) = {
+    val parts = s.split(',')
+    val label = parts(0).toDouble
+    val features = parts.tail.map(replaceEmpty(_)).map(_.toDouble)
+    (label, Vectors.dense(features))
+  }
+
   def main(args: Array[String]) {
+    if (args.length != 1) {
+      println("Usage: WordCrawler <train_data_file>")
+      return
+    }
+
     val conf = new SparkConf().setMaster("local[2]").setAppName("WordCrawler")
     val ssc = new StreamingContext(conf, Seconds(1))
 
+    val trainingData = ssc.textFileStream(args(0)).map(Vectors.parse)
+
     val lines = ssc.socketTextStream("localhost", 1337)
-    lines.flatMap(_.split("\n")).foreachRDD(line => {
-      // TODO: Replace empty values with zeroes
-      //       Parse as vector
-      //       Specify time to train
-      //       Use KMeans
-      // val parsed = line.flatMap(_.split(",")).map(replaceEmpty(_))/* .fold("")(_ ++ "," ++ _) */.map(Vectors.parse)
-      val parsed = line.map(s => Vectors.dense(s.split(',').map(replaceEmpty(_)).map(_.toDouble))).cache()
-      parsed.foreach(println)
-      println("--- batch done ---")
-    })
+    val testData = lines
+      .flatMap(_.split("\n"))
+      .map(toLabelVectorTup)
+      .cache()
+
+    val model = new StreamingKMeans()
+      .setK(3)
+      .setDecayFactor(1.0)
+      .setRandomCenters(4, 0.0)
+
+    model.trainOn(trainingData)
+    model.predictOnValues(testData).print()
 
     ssc.start()
     ssc.awaitTermination()
